0974b6369885fe1ecac6a4fc5bd193b7241d1380
vincent-guizot <vincentguizot128@gmail.com>
2023-05-11 20:28:42 +0700

    1

--- array.js
+++ array.js
@@ -1,10 +1,10 @@
 // Array
 
-var numbers = [5, 2, 3, 1, 7];
+// var numbers = [5, 2, 3, 1, 7];
 
-for (var i = 0; i < numbers.length; i++) {
-  //   console.log(numbers[i]);
-}
+// for (var i = 0; i < numbers.length; i++) {
+//   console.log(numbers[i]);
+// }
 
 var items = ["laptop", "hp", "tablet"];
 // console.log(items)
@@ -37,15 +37,78 @@ var students = [
 // Study Case
 // Hitung jumlah dari semua value / isi array dan cari rata2 nya
 
+// var scores = [5,2,3,1,7,8,10,12,17]
+// var total = 0;
+// var average = 0;
 
-var scores = [5,2,3,1,7,8,10,12,17]
-var total = 0;
-var average = 0;
+// for(var i = 0; i < scores.length; i++) {
+//     total += scores[i]
+// }
 
-for(var i = 0; i < scores.length; i++) {
-    total += scores[i]
+// average = total / scores.length
+// console.log(average.toFixed(2))
+
+// Penggunaan .map
+
+var numbers = [1, 2, 3, 4, 5];
+// Result : [1,8,27,64,125]
+
+// pake for biasa
+var power3 = [];
+for (var i = 0; i < numbers.length; i++) {
+  power3.push(numbers[i] ** 3);
+}
+// console.log(power3);
+
+// pake .map
+var powerThree = numbers.map((number) => number ** 3);
+// console.log(powerThree);
+
+// pake foreach
+
+// numbers.forEach((number) => console.log(number));
+
+// do {} while => di execute terlebih dahulu baru di cek,
+
+// while => di cek terlebih dahulu baru di execute
+
+var items = [
+  {
+    id: 1,
+    name: "laptop"
+  }
+]
+
+var student = {
+  name: "Vincent",
+  hobbies: [
+    "eating", "sleeping"
+  ]
 }
 
-average = total / scores.length
-console.log(average.toFixed(2))
 
+
+// Study Case
+
+/**
+ * Pecah semua karakter dalam string menjadi array
+ * 
+ * Contoh:
+ * var str = "makan",
+ * 
+ * Result:
+ * ['m','a','k','a','n']
+ * 
+ */
+
+// Built In Function str.split()
+
+var str = "makan";
+var result = str.split();
+
+// Cara manual / traditional
+
+var result1 = []
+for(var i = 0; i < str.length; i++){
+  result1.push(str[i])
+}
--- object.js
+++ object.js
@@ -1,32 +1,34 @@
 /// Object
 
 var student = {
-    name: "VIncent",
-    gpa: 3.21,
-    isGraduated: true
-}
+  name: "VIncent",
+  gpa: 3.21,
+  isGraduated: true,
+};
 
 // console.log(student.name)
 // console.log(student.gpa)
 // console.log(student["isGraduated"])
 
 var caca = {
-    name: "Caca",
-    weight: 45,
-    height: 155,
-    hobbies:[
-        'nonton','makan'
-    ],
+  name: "Caca",
+  weight: 45,
+  height: 155,
+  hobbies: ["nonton", "makan"],
 
-    sebutkanHobby: function() {
-        console.log("Hobby caca: ")
-        for(var i = 0; i < this.hobbies.length; i++) {
-            console.log(i+1 + ". " + this.hobbies[i])
-        }
+  sebutkanHobby: function () {
+    console.log("Hobby caca: ");
+    for (var i = 0; i < this.hobbies.length; i++) {
+      console.log(i + 1 + ". " + this.hobbies[i]);
     }
-}
+  },
+  tambahkanHobby: function(hobbyName) {
+    this.hobbies.push(hobbyName)
+  }
+};
 
-caca.sebutkanHobby()
+// caca.tambahkanHobby("coding")
+// caca.sebutkanHobby();
 
 // Study Case
 // Tentukan jumlah huruf vokal dalam suatu kalimat
@@ -42,30 +44,69 @@ caca.sebutkanHobby()
 }
 */
 
-function countVowels(str){
- var result ={
+function countVowels(str) {
+  var result = {
     a: 0,
     i: 0,
     u: 0,
     e: 0,
-    o: 0
- }
+    o: 0,
+  };
 
- for(var i = 0; i < str.length; i++){
-    if(str[i] === 'a'){
-        result.a++;
-    } else if( str[i] ==='i'){
-        result.i++
-    } else if(str[i] ==='e'){
-        result.e++;
-    } else if(str[i] ==='u'){
-        result.u++
-    } else if(str[i] ==='o'){
-        result.o++
+  for (var i = 0; i < str.length; i++) {
+    if (str[i] === "a") {
+      result.a++;
+    } else if (str[i] === "i") {
+      result.i++;
+    } else if (str[i] === "e") {
+      result.e++;
+    } else if (str[i] === "u") {
+      result.u++;
+    } else if (str[i] === "o") {
+      result.o++;
     }
- }
+  }
 
- console.log(result)
+  console.log(result);
 }
 
 countVowels("buka puasa bersama teman kuliah")
+
+// Study Case
+
+// Tentukan voting atau nama orang nilai bilangan yang sering keluar
+
+/**
+ * var votes = ['vincent','vincent','admin']
+ *
+ * Result:
+ * {
+ *      vincent: 2,
+ *      admin: 1
+ * }
+ *
+ * Pemenang nya adalah vincent
+ *
+ * */
+
+function countVotes(arr) {
+  var result = {
+    vincent: 0,
+    admin: 0,
+  };
+
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i].toLowerCase() === "vincent") {
+      result.vincent++;
+    } else if (arr[i].toLowerCase() === "admin") {
+      result.admin++;
+    }
+  }
+
+  console.log(result);
+}
+
+countVotes(["vincent","admin","vincent"])
+// Study Case
+
+// Buat todo apps
